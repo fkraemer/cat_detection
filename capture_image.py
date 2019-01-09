780ccc290b2dc78ea906759fee9521b39444b6b2
julian <julian.frey@posteo.de>
2019-01-09 19:01:54 +0100

    code clean up, gitignore and camera closing for endless loop

--- capture_image.py
+++ capture_image.py
@@ -9,63 +9,61 @@ from cat_dection import DiffDetection
 
 
 class Detection: 
-  def __init__(self, iterations=10):
-    self.dd = DiffDetection()
-    self.oldImg = np.zeros((1,1),np.uint8)
-    self.newImg = np.zeros((1,1),np.uint8)
-    self.iterations = iterations
+    def __init__(self, iterations=10):
+        self.dd = DiffDetection()
+        self.oldImg = np.zeros((1,1),np.uint8)
+        self.newImg = np.zeros((1,1),np.uint8)
+        self.iterations = iterations
 
-  def run(self):
-    #TODO, move to cat_detection and make PiCamera capturing an interfaced camera image getter
+    def run(self):
+        #TODO, move to cat_detection and make PiCamera capturing an interfaced camera image getter
 
-    # initialize the camera and grab a reference to the raw camera capture
-    camera = PiCamera()
+        # initialize the camera and grab a reference to the raw camera capture
+        camera = PiCamera()
 
-    # from https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-consistent-images
-    # Set ISO to the desired value
-    camera.iso = 400
-    # Wait for the automatic gain control to settle
-    time.sleep(2)
-    # Now fix the values
-    camera.shutter_speed = camera.exposure_speed
-    camera.exposure_mode = 'off'
-    g = camera.awb_gains
-    camera.awb_mode = 'off'
-    camera.awb_gains = g
+        try:
+            print  'Auto calibration.'
+            # from https://picamera.readthedocs.io/en/release-1.13/recipes1.html#capturing-consistent-images
+            # Set ISO to the desired value
+            camera.iso = 400
+            # Wait for the automatic gain control to settle
+            time.sleep(2)
+            # Now fix the values
+            camera.shutter_speed = camera.exposure_speed
+            camera.exposure_mode = 'off'
+            g = camera.awb_gains
+            camera.awb_mode = 'off'
+            camera.awb_gains = g
 
 
-    i=0
-    # allow the camera to warmup
-    time.sleep(0.1)
-    while i < self.iterations:
-      i = i + 1
-      self.oldImg = self.newImg.copy()
-      # grab an image from the camera
-      print 'Capturing\n'
-      rawCapture = PiRGBArray(camera)
-      camera.capture(rawCapture, format="bgr")
-      self.newImg = rawCapture.array
-      delta = .0
-      time.sleep(2)
-      ts = time.time()
-      if i > 1:
-        if self.oldImg is None:
-          print "old img is nore more"
-        if self.newImg is None:
-          print "new img is nore more"
-        (diff, delta) = self.dd.differ(self.oldImg, self.newImg,10)
-      else:
-        continue
-      if delta > 0.05:
-        baseString = 'capture_'+str(ts)
-        cv.imwrite(baseString+'_00_before.png', self.oldImg[:,:,2])
-        cv.imwrite(baseString+'_01_after.png', self.newImg[:,:,2])
-        cv.imwrite(baseString+'_02_diff.png', diff)
-      print 'Diff image at time ' + str(ts) + ' had ' + str(delta*100.) + '%'
+            i=0
+            # allow the camera to warmup
+            time.sleep(0.1)
+            while i < self.iterations:
+	            i = i + 1
+	            self.oldImg = self.newImg.copy()
+	            # grab an image from the camera
+	            rawCapture = PiRGBArray(camera)
+	            camera.capture(rawCapture, format="bgr")
+	            self.newImg = rawCapture.array
+	            delta = .0
+	            time.sleep(2)
+	            if i > 1:
+	                (diff, delta) = self.dd.differ(self.oldImg, self.newImg,10)
+	            else:
+	                continue
+	            timeStr = time.strftime('%y_%m_%d_%H_%M_%S')
+	            if delta > 0.05:
+	                cv.imwrite('door_' + timeStr + '_00_before.png', self.oldImg[:,:,2])
+	                cv.imwrite('door_' + timeStr + '_01_after.png', self.newImg[:,:,2])
+	                cv.imwrite('door_' + timeStr + '_02_diff.png', diff)
+	            print 'Diff image at time ' + timeStr + ' had ' + str(delta*100.) + '%'
+        finally:
+            camera.close()
 
 
 
 if __name__ == "__main__":
     while True:
-        d = Detection(1000)
+        d = Detection(10)
         d.run()
