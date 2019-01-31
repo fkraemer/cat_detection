6a64f76b4467b680f7deb0612cc762d1e36633c1
flo <kraemerf@informatik.uni-freiburg.de>
2019-01-31 22:37:58 +0100

    added image read, feature production, train and test pipeline for a classifier

---
+++ cat_detection/CatDetector.py
@@ -0,0 +1,115 @@
+import unittest
+import numpy as np
+import cv2 as cv
+import math
+import os
+import random
+from CatFeatureDetector import CatFeatureDetector
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+class CatDetector:
+    def __init__(self, trainingSetPositive, trainingSetNegative):
+        self._positives = np.array([])
+        self._negatives = np.array([])
+        for catPicSet in trainingSetPositive:
+            detector = catPicSet.getFeatureDetector()
+            featureArray = detector.features()
+            self._positives = featureArray if np.size(self._positives,0) is 0  else np.vstack((self._positives, featureArray ) )
+        for catPicSet in trainingSetNegative:
+            detector = catPicSet.getFeatureDetector()
+            featureArray = detector.features()
+            self._negatives = featureArray if np.size(self._negatives,0) is 0  else np.vstack((self._negatives, featureArray ) )
+
+    def train(self, trainTestSplit):
+        positiveSamplesN = np.size(self._positives,0)
+        negativeSamplesN = np.size(self._negatives,0)
+        labels = np.vstack((np.zeros((negativeSamplesN,1)), np.ones((positiveSamplesN,1)) ) )
+        features = np.vstack( (self._negatives, self._positives) )
+        self._train_x, self._test_x, self._train_y, self._test_y = train_test_split(features, labels, train_size=trainTestSplit, random_state=1)    # Train and Test dataset size details
+        print "Train_x Shape :: ", self._train_x.shape
+        print "Train_y Shape :: ", self._train_y.shape
+        print "Test_x Shape :: ", self._test_x.shape
+        print "Test_y Shape :: ", self._test_y.shape
+        self._clf = RandomForestClassifier(random_state=1)
+        self._clf.fit(self._train_x, self._train_y.flatten())
+        print "Trained model :: ", self._clf
+
+    def test(self):
+        self._predictions = self._clf.predict(self._test_x)
+
+        print "Train Accuracy : ", accuracy_score(self._train_y, self._clf.predict(self._train_x))
+        print "Test Accuracy  : ", accuracy_score(self._test_y, self._predictions)
+        print " Confusion matrix ", confusion_matrix(self._test_y, self._predictions)
+
+
+    def evalFalsePositives(self): # how often did we predict a cat, when there was none
+        cm = confusion_matrix(self._test_y, self._predictions) # y is true label, x is prediction
+        return cm[0,1] / float(cm[0,1] + cm[1,1])
+
+    def evalFalseNegatives(self): # how often did we predict no cat, when there was one
+        cm = confusion_matrix(self._test_y, self._predictions) # y is true label, x is prediction
+        return cm[1,0] / float(cm[0,0] + cm[1,0])
+
+
+
+class CatPicSet:
+
+    def __init__(self, imgBeforePath, imgAfterPath, imgDiffPath):
+        self._imgBeforePath = imgBeforePath
+        self._imgAfterPath = imgAfterPath
+        self._imgDiffPath = imgDiffPath
+
+    def getFeatureDetector(self):
+        imgBefore = cv.imread(self._imgBeforePath, cv.IMREAD_GRAYSCALE)
+        imgAfter = cv.imread(self._imgAfterPath , cv.IMREAD_GRAYSCALE)
+        imgDiff = cv.imread(self._imgDiffPath, cv.IMREAD_GRAYSCALE)
+        return CatFeatureDetector(imgBefore,
+                                  imgAfter,
+                                  imgDiff)
+
+class test(unittest.TestCase):
+
+    def setAggregator(self, path):
+        picSets = list()
+        for root, dirs, files in os.walk(path):
+            for filename in files:
+                if filename.endswith('02_diff.png'):
+                    prefix = filename[:-11]
+                    beforeFilename = prefix + '00_before.png'
+                    afterFilename = prefix + '01_after.png'
+                    if os.path.isfile(path + "/" + beforeFilename) and os.path.isfile(path + "/" + beforeFilename):
+                        picSets.append(CatPicSet(path + "/" + beforeFilename,
+                                                 path + "/" + afterFilename,
+                                                 path + "/" + filename))
+        return picSets
+
+    def testTrainAndClassify(self):
+        basePathPositives = '/home/flo/data/catpics/positive/'
+        basePathNegatives = '/home/flo/data/catpics/negative/'
+        picSetsPositive = self.setAggregator(basePathPositives)
+        picSetsNegatives = self.setAggregator(basePathNegatives)
+        print 'Sets: %d positive vs. %d negatives' % ( len(picSetsPositive), len(picSetsNegatives) )
+        equalSetN = min(len(picSetsPositive), len(picSetsNegatives))
+        trainTestSplit = 0.8
+        trainingN = int(math.floor(equalSetN * trainTestSplit))
+        print 'Balancing. Using %d from each set. %d for training' % ( equalSetN, trainingN )
+
+        #shuffle the list and split into training/test sets
+        random.seed(2)
+        random.shuffle(picSetsPositive)
+        random.shuffle(picSetsNegatives)
+
+        #train
+        cD = CatDetector(picSetsPositive[0:equalSetN], picSetsNegatives[0:equalSetN])
+        cD.train(trainTestSplit)
+        cD.test()
+        falsePositiveScore = cD.evalFalsePositives()
+        print 'false positve score is %f' % (falsePositiveScore*100.,)
+        falseNegativeScore = cD.evalFalseNegatives()
+        print 'false negative score is %f' % (falseNegativeScore*100.,)
+
+        self.assertLess(falsePositiveScore,0.1)
+        self.assertLess(falseNegativeScore,0.5)
--- cat_detection/__init__.py
+++ cat_detection/__init__.py
@@ -1,2 +1,4 @@
 # __init__.py
 from .DiffDetection import DiffDetection
+from .CatFeatureDetector import CatFeatureDetector
+from .CatDetector import CatDetector
